f6498b511486fcebb8e9756efc9a3aaa273667bf
MRastogi5 <118384562+MRastogi5@users.noreply.github.com>
2023-03-04 18:40:53 +0000

    Added new test
    tests to get available , and booked seat count

--- src/main/java/org/cinemas/src/CinnamonCinemas.java
+++ src/main/java/org/cinemas/src/CinnamonCinemas.java
@@ -3,10 +3,17 @@ package org.cinemas.src;
 import java.util.Random;
 
 public class CinnamonCinemas {
-
-
     public int NumberOfSeatRequested() {
         Random rand = new Random();
         return rand.nextInt(3);
+    }
+
+    public int NumberOfSeatAvailable() {
+
+        return 0;
+    }
+
+    public int NumberOfSeatBooked() {
+        return 0;
     }
 }
--- src/test/java/CinnamonCinemasTest.java
+++ src/test/java/CinnamonCinemasTest.java
@@ -1,6 +1,7 @@
 import org.cinemas.src.CinnamonCinemas;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CinnamonCinemasTest {
@@ -10,5 +11,25 @@ public class CinnamonCinemasTest {
         CinnamonCinemas cinnamonCinemas = new CinnamonCinemas();
         assertTrue(cinnamonCinemas.NumberOfSeatRequested()>0);
         assertTrue(cinnamonCinemas.NumberOfSeatRequested()<3);
+    }
+
+    @Test
+    public void testTotalNumberOfSeatBeforeBooking(){
+        CinnamonCinemas cinnamonCinemas = new CinnamonCinemas();
+        assertTrue(cinnamonCinemas.NumberOfSeatAvailable()==15);
+        assertTrue(cinnamonCinemas.NumberOfSeatBooked()<0);
+    }
+
+    @Test
+    public void testTotalNumberOfSeatAvailable(){
+        CinnamonCinemas cinnamonCinemas = new CinnamonCinemas();
+        assertTrue(cinnamonCinemas.NumberOfSeatRequested()==3);
+        assertTrue(cinnamonCinemas.NumberOfSeatAvailable()==12);
+    }
+
+    @Test
+    public void testGetSeatMap(){
+        CinnamonCinemas cinnamonCinemas = new CinnamonCinemas();
+        assertNotNull(cinnamonCinemas.getSeatMap());
     }
 }
