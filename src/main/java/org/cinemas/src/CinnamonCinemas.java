e8f81799c07ffde4ad955689eacd00bc583a6ddb
MRastogi5 <118384562+MRastogi5@users.noreply.github.com>
2023-03-04 19:39:28 +0000

    Added Seat Booking Functionality

--- src/main/java/org/cinemas/src/CinnamonCinemas.java
+++ src/main/java/org/cinemas/src/CinnamonCinemas.java
@@ -8,21 +8,28 @@ public class CinnamonCinemas {
      /*int seatMap[][][] = {{{'A',1},{'A',2},{'A',3},{'A',4},{'A',5}},
                         {{'B',1},{'B',2},{'B',3},{'B',4},{'B',5}},
                         {{'C',1},{'C',2},{'C',3},{'C',4},{'C',5}}};*/
-     private static String seatMap[][] = {  {"A1","A2","A3","A4","A5"},
-                            {"B1","B2","B3","B4","B5"},
-                            {"C1","C2","C3","C4","C5"}};
+     private static String seatMap[] = {  "A1","A2","A3","A4","A5",
+                            "B1","B2","B3","B4","B5",
+                            "C1","C2","C3","C4","C5"};
 
     private static int noOfSeatBooked = 0;
-    private static int noOfSeatAvailable =0;
-    public final static int noOfSeatTotal =15;
+    private static int noOfSeatAvailable = 15;
+    public final static int noOfSeatTotal = 15;
 
-    public String[][] getSeatMap() {
+    public String getSeatLastBooked() {
+        return seatLastBooked;
+    }
+
+    public void setSeatLastBooked(String seatLastBooked) {
+        this.seatLastBooked = seatLastBooked;
+    }
+
+    private String seatLastBooked = "None";
+
+    public String[] getSeatMap() {
         return seatMap;
     }
-    public int NumberOfSeatRequested() {
-        Random rand = new Random();
-        return rand.nextInt(3);
-    }
+
     public int NumberOfSeatAvailable() {
         System.out.println(noOfSeatTotal - noOfSeatBooked);
         return noOfSeatTotal - noOfSeatBooked;
@@ -30,5 +37,35 @@ public class CinnamonCinemas {
 
     public int NumberOfSeatBooked() {
         return noOfSeatBooked;
+    }
+
+    // Assumption : 0 < noOfSeats <=3
+    public boolean bookSeats(int i) {  // return seat numbers  noOfSeats
+
+        System.out.println("No. Of Seats Available :  "+noOfSeatAvailable);
+        System.out.println("No. Of Seats Booked :  "+noOfSeatBooked);
+        System.out.println("seatLastBooked :  "+seatLastBooked);
+        System.out.println("No. Of Seats Requested :  "+i);
+
+        if(noOfSeatAvailable >= i) {
+           // for (int row = 0; row < 15; row++) {
+                noOfSeatBooked = noOfSeatBooked + i;
+                noOfSeatAvailable = noOfSeatAvailable - noOfSeatBooked;
+            //}
+        } else {
+            System.out.println("Sorry!!! Requested number of seats are not available. ");
+            return false;
+        }
+
+        seatLastBooked = seatMap[noOfSeatBooked -1];
+
+        System.out.println("No. Of Seats Available Now :  "+noOfSeatAvailable);
+        System.out.println("No. Of Seats Booked Now :   :  "+noOfSeatBooked);
+        System.out.println("seatLastBooked Now :  "+seatLastBooked);
+
+        System.out.println("*********************************************");
+
+        //System.out.print("seatLastBooked :  "+seatLastBooked);
+        return true;
     }
 }
--- src/test/java/CinnamonCinemasTest.java
+++ src/test/java/CinnamonCinemasTest.java
@@ -1,35 +1,55 @@
 import org.cinemas.src.CinnamonCinemas;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CinnamonCinemasTest {
 
+    CinnamonCinemas cinnamonCinemas = new CinnamonCinemas();
+
+    public int NumberOfSeatRequested() {
+        Random rand = new Random();
+        return rand.nextInt(3);
+    }
     @Test
     public void testNumberOfSeatRequested(){
-        CinnamonCinemas cinnamonCinemas = new CinnamonCinemas();
-        assertTrue(cinnamonCinemas.NumberOfSeatRequested()>=0);
-        assertTrue(cinnamonCinemas.NumberOfSeatRequested()<=3);
+        //CinnamonCinemas cinnamonCinemas = new CinnamonCinemas();
+        assertTrue(NumberOfSeatRequested()>=0);
+        assertTrue(NumberOfSeatRequested()<=3);
     }
 
     @Test
     public void testTotalNumberOfSeatBeforeBooking(){
-        CinnamonCinemas cinnamonCinemas = new CinnamonCinemas();
+        //CinnamonCinemas cinnamonCinemas = new CinnamonCinemas();
         assertTrue(cinnamonCinemas.NumberOfSeatAvailable()==15);
         assertTrue(cinnamonCinemas.NumberOfSeatBooked()<=0);
     }
 
     @Test
     public void testTotalNumberOfSeatAvailable(){
-        CinnamonCinemas cinnamonCinemas = new CinnamonCinemas();
+        //CinnamonCinemas cinnamonCinemas = new CinnamonCinemas();
         assertTrue(cinnamonCinemas.NumberOfSeatAvailable()<=15);
         assertTrue(cinnamonCinemas.NumberOfSeatAvailable()>=0);
     }
 
     @Test
     public void testGetSeatMap(){
-        CinnamonCinemas cinnamonCinemas = new CinnamonCinemas();
+        //CinnamonCinemas cinnamonCinemas = new CinnamonCinemas();
         assertNotNull(cinnamonCinemas.getSeatMap());
+    }
+    @Test
+    public void testBookSeats(){
+        while(true) {
+            int NumberOfSeatRequested = NumberOfSeatRequested();
+            if(cinnamonCinemas.bookSeats(NumberOfSeatRequested) == false){
+                break;
+            }
+            else {
+                assertTrue(cinnamonCinemas.bookSeats(NumberOfSeatRequested));
+            }
+        }
     }
 }
