fb6315166b868e9124572c944ec2fd995af4b5e4
MRastogi5 <118384562+MRastogi5@users.noreply.github.com>
2023-03-04 02:07:38 +0000

    Update CinnamonCinemas.java

--- src/main/java/org/cinemas/src/CinnamonCinemas.java
+++ src/main/java/org/cinemas/src/CinnamonCinemas.java
@@ -1,6 +1,12 @@
 package org.cinemas.src;
 
+import java.util.Random;
+
 public class CinnamonCinemas {
 
 
+    public int NumberOfSeatRequested() {
+        Random rand = new Random();
+        return rand.nextInt(3);
+    }
 }
